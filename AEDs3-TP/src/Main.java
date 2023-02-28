b1549c4f7ca43cecae25608a0e4d298292369a59
gabriel <gabriel@gabriel-Default-string>
2023-02-28 19:34:40 -0300

    a

--- AEDs3-TP/src/Main.java
+++ AEDs3-TP/src/Main.java
@@ -7,7 +7,7 @@ import java.text.SimpleDateFormat;
 
 public class Main {
     public static void main(String[] args) {
-        String csvFile = "movies.csv/./";
+        String csvFile = "movies.csv";
         String line = "";
         String cvsSplitBy = ",";
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
