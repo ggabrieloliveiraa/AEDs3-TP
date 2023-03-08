9cb2da3c89ae5f66fb508d57df3d43e869c1f6d0
waands <wanderjunior36@gmail.com>
2023-03-08 16:57:13 -0300

    data inserir

--- AEDs3-TP/src/Main.java
+++ AEDs3-TP/src/Main.java
@@ -162,7 +162,7 @@ public class Main {
 		System.out.println("Digite a avaliação do filme, separado por vírgula");
 		rating = sc.nextFloat();
 
-		System.out.println("Digite o ano de lançamento do filme");
+		System.out.println("Digite o ano de lançamento do filme (YYYY)");
 		int date = sc.nextInt();
 		//Date year = new Date();// por enquanto deixei assim(atributos[1])
 		//id = 10064;
