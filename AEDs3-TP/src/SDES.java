05a55731be74c2b51442633e724a4674b2ee0cf6
waands <wanderjunior36@gmail.com>
2023-06-18 21:13:51 -0300

    comentarios SDES
    tinha esquecido de colocar cometarios SDES

--- AEDs3-TP/src/SDES.java
+++ AEDs3-TP/src/SDES.java
@@ -1,19 +1,36 @@
 import java.util.BitSet;
-    import java.util.Arrays;
+import java.util.Arrays;
 
 public class SDES {
+    // Tabelas de permutação e substituição utilizadas no algoritmo
+
+    // Tabela de permutação P10
     private static final int[] P10 = { 3, 5, 2, 7, 4, 10, 1, 9, 8, 6 };
+    
+    // Tabela de permutação P8
     private static final int[] P8 = { 6, 3, 7, 4, 8, 5, 10, 9 };
+    
+    // Tabela de permutação IP (Permutação Inicial)
     private static final int[] IP = { 2, 6, 3, 1, 4, 8, 5, 7 };
+    
+    // Tabela de permutação EP (Permutação de Expansão)
     private static final int[] EP = { 4, 1, 2, 3, 2, 3, 4, 1 };
+    
+    // Tabela de permutação P4
     private static final int[] P4 = { 2, 4, 3, 1 };
+    
+    // Tabela de permutação IP^-1 (Permutação Inversa da Permutação Inicial)
     private static final int[] IP_INVERSE = { 4, 1, 3, 5, 7, 2, 8, 6 };
+    
+    // Matriz de substituição S0
     private static final int[][] S0 = {
             { 1, 0, 3, 2 },
             { 3, 2, 1, 0 },
             { 0, 2, 1, 3 },
             { 3, 1, 3, 2 }
     };
+    
+    // Matriz de substituição S1
     private static final int[][] S1 = {
             { 0, 1, 2, 3 },
             { 2, 0, 1, 3 },
@@ -26,6 +43,7 @@ public class SDES {
     private BitSet subKey2;
 
     public SDES() {
+        // Construtor padrão, configura a chave padrão para o algoritmo
         subKey1 = new BitSet(8);
         subKey2 = new BitSet(8);
 
@@ -37,61 +55,59 @@ public class SDES {
         key.set(9);
         setKey(key);
     }
-    /*
-    public String encrypt (String tmp){
-        return bitSetToString(encrypt(stringToBitSet(tmp)));
-    }
-    public String decrypt (String tmp){
-        return bitSetToString(decrypt(stringToBitSet(tmp)));
-    }
-    */
 
     public String encrypt(String plaintext) {
+        // Método para criptografar uma sequência de texto
         BitSet bitSet = stringToBitSet(plaintext);
         BitSet ciphertext = encrypt(bitSet);
         return bitSetToString(ciphertext);
     }
 
     public String decrypt(String ciphertext) {
+        // Método para descriptografar uma sequência de texto criptografado
         BitSet bitSet = stringToBitSet(ciphertext);
         BitSet plaintext = decrypt(bitSet);
         return bitSetToString(plaintext);
     }
 
     public BitSet encrypt(BitSet plaintext) {
+        // Método para criptografar um BitSet
         generateSubKeys();
 
         BitSet permutedPlaintext = permute(plaintext, IP);
-        BitSet fkResult = fk(permutedPlaintext, subKey1);
-        BitSet swapped = swapBits(fkResult);
-        BitSet fkResult2 = fk(swapped, subKey2);
-        BitSet ciphertext = permute(fkResult2, IP_INVERSE);
+        BitSet fkOutput1 = fk(permutedPlaintext, subKey1);
 
-        return ciphertext;
+        BitSet swapped = swapBits(fkOutput1);
+        BitSet fkOutput2 = fk(swapped, subKey2);
+
+        BitSet encrypted = permute(fkOutput2, IP_INVERSE);
+
+        return encrypted;
     }
 
-    
-
     public BitSet decrypt(BitSet ciphertext) {
+        // Método para descriptografar um BitSet criptografado
         generateSubKeys();
 
         BitSet permutedCiphertext = permute(ciphertext, IP);
-        BitSet fkResult = fk(permutedCiphertext, subKey2);
-        BitSet swapped = swapBits(fkResult);
-        BitSet fkResult2 = fk(swapped, subKey1);
-        BitSet plaintext = permute(fkResult2, IP_INVERSE);
+        BitSet fkOutput1 = fk(permutedCiphertext, subKey2);
 
-        return plaintext;
+        BitSet swapped = swapBits(fkOutput1);
+        BitSet fkOutput2 = fk(swapped, subKey1);
+
+        BitSet decrypted = permute(fkOutput2, IP_INVERSE);
+
+        return decrypted;
     }
 
     public void setKey(BitSet key) {
-        if (key.length() != 10) {
-            throw new IllegalArgumentException("Invalid key length. Key must be 10 bits long.");
-        }
+        // Método para definir a chave utilizada pelo algoritmo
         this.key = key;
+        generateSubKeys();
     }
 
     private void generateSubKeys() {
+        // Método para gerar as subchaves a partir da chave principal
         BitSet permutedKey = permute(key, P10);
         BitSet leftKey = leftShift(permutedKey, 1);
         BitSet rightKey = leftShift(permutedKey, 2);
@@ -100,6 +116,7 @@ public class SDES {
     }
 
     private BitSet permute(BitSet input, int[] permutationTable) {
+        // Método para realizar uma permutação em um BitSet de acordo com uma tabela de permutação
         BitSet output = new BitSet(permutationTable.length);
         for (int i = 0; i < permutationTable.length; i++) {
             output.set(i, input.get(permutationTable[i] - 1));
@@ -108,6 +125,7 @@ public class SDES {
     }
 
     private BitSet leftShift(BitSet input, int count) {
+        // Método para realizar um deslocamento à esquerda em um BitSet
         BitSet output = (BitSet) input.clone();
         output.clear(0, count);
         for (int i = input.length() - count; i < input.length(); i++) {
@@ -117,6 +135,7 @@ public class SDES {
     }
 
     private BitSet fk(BitSet input, BitSet subKey) {
+        // Método para realizar a função de Feistel (FK)
         BitSet expanded = permute(input, EP);
         expanded.xor(subKey);
 
@@ -143,6 +162,7 @@ public class SDES {
     }
 
     private BitSet sBox(BitSet left, BitSet right) {
+        // Método para realizar as substituições S-Box
         int row1 = getDecimalValue(left.get(0), left.get(3));
         int col1 = getDecimalValue(left.get(1), left.get(2));
         int sBox1Value = S0[row1][col1];
@@ -151,60 +171,63 @@ public class SDES {
         int col2 = getDecimalValue(right.get(1), right.get(2));
         int sBox2Value = S1[row2][col2];
 
-        BitSet output = new BitSet(4);
-        output.set(0, (sBox1Value & 0b10) != 0);
-        output.set(1, (sBox1Value & 0b01) != 0);
-        output.set(2, (sBox2Value & 0b10) != 0);
-        output.set(3, (sBox2Value & 0b01) != 0);
+        BitSet sBoxOutput = new BitSet(4);
+        sBoxOutput.set(0, (sBox1Value & 2) != 0);
+        sBoxOutput.set(1, (sBox1Value & 1) != 0);
+        sBoxOutput.set(2, (sBox2Value & 2) != 0);
+        sBoxOutput.set(3, (sBox2Value & 1) != 0);
 
-        return output;
+        return sBoxOutput;
     }
 
     private int getDecimalValue(boolean bit1, boolean bit2) {
-        int value = 0;
-        if (bit1) value += 2;
-        if (bit2) value += 1;
-        return value;
+        // Método para obter o valor decimal correspondente a dois bits
+        if (bit1 && bit2) {
+            return 3;
+        } else if (bit1) {
+            return 2;
+        } else if (bit2) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
     private BitSet swapBits(BitSet input) {
-        BitSet output = new BitSet(input.length());
-        for (int i = 0; i < input.length(); i++) {
-            output.set(i, input.get(i + (input.length() / 2)));
-        }
-        for (int i = input.length() / 2; i < input.length(); i++) {
-            output.set(i, input.get(i - (input.length() / 2)));
-        }
+        // Método para trocar os bits do lado esquerdo pelos bits do lado direito
+        BitSet output = new BitSet(8);
+        output.set(0, input.get(4));
+        output.set(1, input.get(5));
+        output.set(2, input.get(6));
+        output.set(3, input.get(7));
+        output.set(4, input.get(0));
+        output.set(5, input.get(1));
+        output.set(6, input.get(2));
+        output.set(7, input.get(3));
         return output;
     }
-    private static BitSet stringToBitSet(String text) {
-        BitSet bitSet = new BitSet(text.length() * 8);
-        for (int i = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
-            for (int j = 0; j < 8; j++) {
-                bitSet.set(i * 8 + j, ((c >> (7 - j)) & 1) == 1);
+
+    private BitSet stringToBitSet(String input) {
+        // Método para converter uma sequência de texto em um BitSet
+        byte[] bytes = input.getBytes();
+        BitSet bitSet = new BitSet(bytes.length * 8);
+        int bitIndex = 0;
+        for (int i = 0; i < bytes.length; i++) {
+            for (int j = 7; j >= 0; j--) {
+                bitSet.set(bitIndex++, (bytes[i] >> j & 1) == 1);
             }
         }
-        System.out.println("stringToBitSet = " + bitSet);
         return bitSet;
     }
 
-
-
-    private static String bitSetToString(BitSet bitSet) {
-        int byteArraySize = (bitSet.length() + 7) / 8;
-        byte[] byteArray = new byte[byteArraySize];
-        for (int i = 0; i < bitSet.length(); i++) {
-            if (bitSet.get(i)) {
-                int byteIndex = i / 8;
-                int bitIndex = 7 - (i % 8);
-                byteArray[byteIndex] |= (1 << bitIndex);
+    private String bitSetToString(BitSet input) {
+        // Método para converter um BitSet em uma sequência de texto
+        byte[] bytes = new byte[(input.length() + 7) / 8];
+        for (int i = 0; i < input.length(); i++) {
+            if (input.get(i)) {
+                bytes[i / 8] |= 1 << (7 - (i % 8));
             }
         }
-        String str = new String(byteArray);
-        System.out.println("String: " + str);
-        System.out.println("bitSetToString = " + Arrays.toString(byteArray));
-        return new String(byteArray);
+        return new String(bytes);
     }
-
 }
